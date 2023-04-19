5fba64591929365815e0c83c894b045be4a62480
Dhruv Chawla <84620619+dhruxchawla@users.noreply.github.com>
2023-04-19 22:01:58 +0530

    Commit

---
+++ components/ProductElement.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from 'next/link'
+
+const styles = {
+  card: "bg-white border",
+  summary: "px-4 py-2 border-gray-100 bg-gray-50 border-t",
+  title: "block text-lg text-gray-900 truncate",
+  category: "block text-sm font-medium text-gray-500",
+  image: {
+    aspect: "aspect-h-1 aspect-w-1",
+    content: "object-center object-cover",
+  },
+};
+
+import { Product } from "@/saleor/api";
+
+type Props = Pick<Product, "id" | "name" | "thumbnail" | "category">;
+
+export const ProductElement = ({ id, name, thumbnail, category }: Props) => {
+  return (
+    <li key={id} className={styles.card}>
+         <Link href={`/product/${id}`} legacyBehavior>
+      <a>
+        <div className={styles.image.aspect}>
+          <img src={thumbnail?.url} alt="" className={styles.image.content} />
+        </div>
+        <div className={styles.summary}>
+          <p className={styles.title}>{name}</p>
+          <p className={styles.category}>{category?.name}</p>
+        </div>
+      </a>
+      </Link>
+    </li>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+import { apolloClient } from '@/lib';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
